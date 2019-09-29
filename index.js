91bb306d6ee47791eab8765daf0db6f5227781ca
ban hussein <banhussien97@gmail.com>
2019-09-29 17:35:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const testVar = {}
 
 function testFunc() {
   return "hi"
+};
+
+function superbowlWin(record){
+let o=record.find(record => record.result === "W");
+
+if(o === undefined)
+{
+  return o
+}
+else{return o.year}
+
 }
