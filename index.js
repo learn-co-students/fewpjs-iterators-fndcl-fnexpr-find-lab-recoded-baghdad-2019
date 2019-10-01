32e0998000ed2cec5638b3fc3690980c7276ac0d
Mohammed Saeb <mohammed.saeb98@gmail.com>
2019-10-01 20:34:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+/*
+function superbowlWin(obj){
+ for (event of obj){
+   if (event.result == 'W') {
+        return event.year
+        }
+ }
 }
+
+superbowlWin(record)
+*/
+
+let record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(obj){
+  let winner = obj.find(res => res.result == 'W')
+  if (winner){
+    return winner.year
+  } else {
+    return undefined
+  }
+}
+
+// winner = record.find(superbowlWin)
