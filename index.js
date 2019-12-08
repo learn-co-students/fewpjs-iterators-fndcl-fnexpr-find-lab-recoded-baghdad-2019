05865af600d8f4e5b6b06e9c77dc18b9c7700885
ibraheem Hasan <rules656@gmail.com>
2019-12-08 17:59:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record){
+let x=record.find(record=>record.result==="W");
+if(x === undefined)
+{
+ return x
+}
+else{
+  return x.year
+  
+}
+}
