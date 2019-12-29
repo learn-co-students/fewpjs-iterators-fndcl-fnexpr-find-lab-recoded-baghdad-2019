f702b33618975dc536c05325a0222d4fa793b081
Mohammed Ali <dody200039@gmail.com>
2019-12-29 23:10:45 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record){
+let r=record.find(record=>record.result==="W");
+if(r === undefined)
+{
+ return r
+}
+else{
+  return r.year
+  
+}
+}
