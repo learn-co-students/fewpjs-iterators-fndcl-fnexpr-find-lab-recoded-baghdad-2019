a44d6256f5ef488738550454912768cadb79a2c2
Sura-93 <suraaljailawi@gmailcom>
2020-01-17 15:14:27 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+];
+function superbowlWin(arr) {
+  let findWin = arr.find(function(w) {
+    return w["result"] === "W";
+  });
+  if (findWin === undefined) {
+    return findWin;
+  } else {
+    return findWin["year"];
+  }
 }
